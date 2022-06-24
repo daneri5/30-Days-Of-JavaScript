ac09bcf10201e254b21af2bbc542aa9bc774a7e0
daneri5 <107131187+daneri5@users.noreply.github.com>
2022-06-24 03:20:00 +0300

    Giriş
    giriş bölümünde anlatılanı kendime göre yazdım

---
+++ giriş.js
@@ -0,0 +1,168 @@
+//  CONSOLE KULLANIMI
+                                                                                                        /*
+console.log("taner")
+alert("taner")
+document.write("taner")
+                                                                                                        */
+
+//// ******************************************************************************************************************
+
+//  TYPE OF OPERATÖRÜ
+                                                                                                        /*
+console.log(typeof 'Asabeneh')   // string
+console.log(typeof 5)            // number
+console.log(typeof true)         // boolean
+console.log(typeof null)         // object type
+console.log(typeof undefined)    // undefined
+
+                                                                                                        */
+//// ******************************************************************************************************************
+                                                                                                        /*
+let firsName = 'taner'
+
+//let               anahtar sözcük
+//firstName         değişken veriable
+// =                atama operatörü
+// 'taner'          atanmış veri, değer value
+                                                                                                        */
+//// ******************************************************************************************************************
+
+// ÖRNEK 1
+
+                                                                                                        /*
+
+let isim              =      "taner"    ,
+    esİsim            =      "Merve"    ,
+    cocukİsim         =      "Karya"    ,
+    halaEvliMi        =       true      ,
+    kacYillikEvli     =       7         ,
+    kiraNeKadar       =       null       ,
+    kazancNeKadar     =       undefined
+
+console.log(isim , esİsim , cocukİsim , halaEvliMi, kacYillikEvli, kiraNeKadar,kazancNeKadar) // taner , merve ,karya , vs
+                                                                                                        */
+
+//// ******************************************************************************************************************
+// ÖRNEK 2
+                                                                                                       /* 
+                                                                                                        
+let numOne = 1
+let numTwo = 3
+let numThree = 3
+
+console.log ( numOne == numTwo)         //false
+console.log ( numTwo == numThree)       //true
+                                                                                                        */
+
+//// ******************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let asd = [ 1 , 2 , "hello" ]
+
+asd[2] =3
+
+console.log(asd)*/
+
+//************************************************************************
+
+/*let taner = "merve"
+let karya = "merve"
+console.log (taner)
+console.log(karya)
+
+let mervee = ["karya" , "taner", 3]
+
+mervee [2]= "merve"
+
+console.log(mervee)*/
+
+//// ******************************************************************************************************************
+
+/*
+const PI = Math.PI 
+
+console.log(PI)    
+*/
+
+//// ******************************************************************************************************************
+/*
+let isim = "Taner" ,
+    soyisim = "ipek",
+    yas = 34,
+    sehir = "izmir",
+    esi = "Merve",
+    cocuk = "Karya",
+    boy = 1.68,
+    kilo = 74,
+    ayak = 43
+
+    let fullname = "ben " + isim + " " +soyisim +"."+ yas+ " yaşındayım." + " "+ sehir +" de oturuyorum."
+    +" eşimin adı "+ esi+ " , "+" cocugumun adı "+ cocuk+"." + boy +" boyunda ,"+ kilo +" kiloyum. " + " ayak numaram "+ayak 
+
+    console.log ( fullname )*/
+
+    // yukarıda ki artık yok 
+/*
+    let fullname = `ben ${isim} ${soyisim}. ${yas} yaşındayım. ${sehir}de oturuyorum. eşimin adı ${esi} , çocuğumun adı ${cocuk}. boy ${boy} ,${kilo} kiloyum. ${ayak} ayak`
+
+    console.log(fullname)*/
+/*
+    let a = "mehmet"
+    let b = "ahmet"
+
+   let fullname = `a+b = ${a+b}`
+console.log(fullname)
+
+//*****************************************************************************************
+
+/*
+let paragraf = ` merhaba\n
+ben\n
+taner\n
+sen\n
+kimsin? `
+
+console.log(paragraf)*/
+
+//************************************************* */
+
+
